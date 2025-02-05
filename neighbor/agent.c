997fa8401a2992335bf4d601d48fa2539ebb87a0
Warsgo <119425679+Warsgo@users.noreply.github.com>
2025-02-05 16:32:07 +0100

    Create agent.c

---
+++ neighbor/agent.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+#define BROADCAST_IP "255.255.255.255"  // Adresse de diffusion globale
+
+int main() {
+    int server_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char hostname[BUFFER_SIZE];
+
+    gethostname(hostname, BUFFER_SIZE);
+
+    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Agent en écoute sur le port %d...\n", PORT);
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int len = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (len < 0) {
+            perror("recvfrom");
+            exit(EXIT_FAILURE);
+        }
+
+        buffer[len] = '\0';
+
+        // Extraire le nombre de sauts de la requête
+        int hops;
+        sscanf(buffer, "DISCOVER %d", &hops);
+
+        // Envoyer le nom de la machine
+        sendto(server_fd, hostname, strlen(hostname), 0, (struct sockaddr *)&client_addr, client_addr_len);
+       ("Nom de la machine envoyé: %s\n", hostname);
+
+        // Propager la requête si le nombre de sauts est supérieur à 1
+        if (hops > 1) {
+            struct sockaddr_in broadcast_addr;
+            broadcast_addr.sin_family = AF_INET;
+            broadcast_addr.sin_port = htons(PORT);
+            broadcast_addr.sin_addr.s_addr = inet_addr(BROADCAST_IP);
+
+            // Préparer le message de découverte avec le nombre de sauts décrémenté
+            snprintf(buffer, BUFFER_SIZE, "DISCOVER %d", hops - 1);
+
+            // Envoyer la requête de découverte
+            if (sendto(server_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&broadcast_addr, sizeof(broadcast_addr)) < 0) {
+                perror("sendto");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Requête de découverte propagée avec %d sauts\n", hops - 1);
+        }
+    }
+
+    return 0;
+}
